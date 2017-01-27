c5e140d0408b92dd24def96ce50d32b03094553e
Alejandro PRUDENTE <alejandro.prudente@gmail.com>
2017-01-27 16:13:36 +0100

    New bootswatch theme.

--- GameGroupManager/App_Start/BundleConfig.cs
+++ GameGroupManager/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace GameGroupManager
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
+					  "~/Content/united.bootstrap.css",
 					  "~/Content/site.css"));
 		}
 	}
